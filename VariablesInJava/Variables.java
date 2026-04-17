0908f0293275278606ec8be02585d3ef9f5809f7
ShreyashPrateek <shreyashprateek01@gmail.com>
2026-04-17 11:56:26 +0530

    Variables Basics

---
+++ VariablesInJava/Variables.java
@@ -0,0 +1,19 @@
+package VariablesInJava;
+
+public class Variables {
+    public static void main(String[] args) {
+        
+        // Creating Variables in Java
+        int problemsSolved; // Declaration
+        problemsSolved = 5; // Assignment 
+        System.out.println(problemsSolved);
+
+        // Changing Variable Values
+        // The value stored inside a variable can change.
+
+        int rank = 450;
+        // after next contest
+        rank = 320;
+        System.out.println(rank);
+    }
+}
